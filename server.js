4c386a5b3ff8703d31b750c738adfec7975cb0d3
Karthikeyan <karthikeyan@Karthikeyans-MacBook-Air.local>
2022-02-20 14:25:05 +0530

    backend build.

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 const express = require("express");
 const cors = require("cors"); // cors for security-purpose it allows to connect this app with other apps!!
 
+const port = process.env.PORT || 8000;
 const app = express(); // server created
 app.use(cors());        // using cors
 
 const userRouter = require("./routers/index"); // miniexpress server imported here
 app.use("/api/v1", userRouter); // miniexpress server used here
 
-app.listen(8000, () => {
+app.listen(port, () => {
   // here we create the sever to run in the port 8000
   console.log("Application has started successfully!!!. Hurrah!!"); 
 });
